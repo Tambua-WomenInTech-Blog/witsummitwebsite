ff0c9aef87acad129af80779dbbd312db4b10e99
JosephineAkello <josephineakello2030@gmail.com>
2026-09-02 14:32:38 +0200

    update note

--- src/app/sections/agenda-section.tsx
+++ src/app/sections/agenda-section.tsx
@@ -358,7 +358,7 @@ const WITSummitAgenda: React.FC<WITSummitAgendaProps> = ({
                 d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
               />
             </svg>
-            Download Previous Agenda(2025)
+            Download Agenda(DRAFT)
           </button>
         </div>
 
